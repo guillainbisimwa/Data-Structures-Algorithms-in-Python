a16c84ddec88a2751073beafb23ba2896f84f911
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-02-28 22:31:10 +0200

    Finish Hash table

--- 4-Maps-and-Hashing/HashTable.py
+++ 4-Maps-and-Hashing/HashTable.py
@@ -9,18 +9,31 @@ class HashTable(object):
     def store(self, string):
         """Input a string that's stored in 
         the table."""
-        pass
+        hv = self.calculate_hash_value(string)
+        # print "STORE: "+str(hv)
+        if hv != -1:
+            if self.table[hv] != None:
+                self.table[hv].append(string)
+            else:
+                self.table[hv] = [string]
 
     def lookup(self, string):
         """Return the hash value if the
         string is already in the table.
         Return -1 otherwise."""
+        hv = self.calculate_hash_value(string)
+        # print "Look up: "+str(hv)
+        if hv != -1:
+            if self.table[hv] != None:
+                if string in self.table[hv]:
+                    return hv
         return -1
 
     def calculate_hash_value(self, string):
         """Helper function to calulate a
         hash value from a string."""
-        return -1
+        value = ord(string[0])*100 + ord(string[1])
+        return value
     
 # Setup
 hash_table = HashTable()
