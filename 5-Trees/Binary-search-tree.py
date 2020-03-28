266d037c47ce3cd212cdc18b37a337882c103b00
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-03-28 17:14:23 +0200

    Binary Search tree

--- 5-Trees/Binary-search-tree.py
+++ 5-Trees/Binary-search-tree.py
@@ -9,11 +9,33 @@ class BST(object):
         self.root = Node(root)
 
     def insert(self, new_val):
-        
-        pass
+        self.inserting_value(tree.root, new_val)        
+    
+    def inserting_value(self, start, new_val):
 
+        if start:
+            if start.value < new_val:
+                if start.left != None:
+                    self.inserting_value(start.left, new_val)
+                else:
+                    start.left = Node(new_val)
+            else:
+                if start.right != None:
+                    self.inserting_value(start.right, new_val)
+                else:
+                    start.right = Node(new_val)
+                    
     def search(self, find_val):
-        return False
+        return self.recursive_search(tree.root, find_val)
+    
+    def recursive_search(self, start, find_val):
+        if start:
+            if start.value == find_val:
+                return True
+            else:
+                return self.recursive_search(start.left, find_val) or self.recursive_search(start.right, find_val)
+        else:
+            return False
     
 # Set up tree
 tree = BST(4)
