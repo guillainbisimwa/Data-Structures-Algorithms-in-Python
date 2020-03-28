6845409adb0332fddb2273804b6e268cecc9ac7b
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-03-28 16:42:04 +0200

    Init BST

---
+++ 5-Trees/Binary-search-tree.py
@@ -0,0 +1,31 @@
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BST(object):
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def insert(self, new_val):
+        
+        pass
+
+    def search(self, find_val):
+        return False
+    
+# Set up tree
+tree = BST(4)
+
+# Insert elements
+tree.insert(2)
+tree.insert(1)
+tree.insert(3)
+tree.insert(5)
+
+# Check search
+# Should be True
+print tree.search(4)
+# Should be False
+print tree.search(6)
